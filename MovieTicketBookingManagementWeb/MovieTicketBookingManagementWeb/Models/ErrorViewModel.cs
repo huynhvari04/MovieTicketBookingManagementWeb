699dc0c42408a07ecd911e07fc9abc3ba372c349
huynhvari04 <varihuynh@gmail.com>
2025-03-18 15:35:59 +0700

    Delete MovieTicketBookingManagementWeb/MovieTicketBookingManagementWeb directory

--- MovieTicketBookingManagementWeb/MovieTicketBookingManagementWeb/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MovieTicketBookingManagementWeb.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
